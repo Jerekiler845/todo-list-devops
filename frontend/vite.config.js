d884de3cac4216c3ae2e8970df5b2857a7602356
2107117@ucc.edu.ar <jtomllenovich@hotmail.com.ar>
2025-11-14 11:31:06 -0300

    arreglo vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    allowedHosts: ['todo-frontend-qa.onrender.com'], // 🔥 AGREGADO
   },
   build: {
     outDir: 'build',
